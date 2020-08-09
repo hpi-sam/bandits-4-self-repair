aaeec1341738eac25ae1844e5dc77129c794a345
leonhard.hennicke <leonhard.hennicke@student.hpi.de>
2020-08-09 22:34:40 +0200

    Executing Experiments.

--- envs/broken_components.py
+++ envs/broken_components.py
@@ -7,24 +7,31 @@ from gym import spaces
 
 from envs.data_handler import DataHandler
 
-DATA_HANDLER = DataHandler()
-
 
 class BrokenComponentsEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, broken_components: List[Tuple], reward_modus: str = 'raw', reward_decrease: bool = False, reward_decrease_factor: float = 0.99):
+    def __init__(self, broken_components: List[Tuple], reward_modus: str = 'raw', reward_decrease: bool = False,
+                 reward_decrease_factor: float = 0.99, state_as_vec=False,
+                 dh_data_generation: str = 'Linear', dh_take_component_id: bool = True, dh_distinguishable: bool = False):
         super(BrokenComponentsEnv, self).__init__()
-        self.data_handler = DATA_HANDLER
+        self.dh_data_generation = dh_data_generation
+        self.dh_take_component_id = dh_take_component_id
         self.reward_modus = reward_modus
+        self.dh_distinguishable = dh_distinguishable
+        self.data_handler = DataHandler(data_generation=self.dh_data_generation,
+                                        take_component_id=self.dh_take_component_id,
+                                        transformation=self.reward_modus,
+                                        distinguishable=self.dh_distinguishable)
         self.reward_decrease = reward_decrease
         self.reward_decrease_factor = reward_decrease_factor
         self.punishment = self.data_handler.data.max()*-1
         self.action_space, self.action_space_names = self.__create_action_space(broken_components)
         self.observation_space, self.observation_space_names = self.__create_observation_space(broken_components)
         self.observation_name_dict = self.__create_map(self.observation_space_names)
+        self.state_as_vec = state_as_vec
 
-        # inital state and action
+        # initial state and action
         self.reset()
 
     def __create_action_space(self, broken_components: List[Tuple]) -> Tuple[gym.spaces.Discrete, np.array]:
@@ -36,8 +43,8 @@ class BrokenComponentsEnv(gym.Env):
     def __create_observation_space(self, broken_components: List[Tuple]) -> Tuple[gym.spaces.Discrete, np.array]:
         broken_components_names = np.empty(len(broken_components), dtype=object)
         broken_components_names[:] = broken_components
-        masks = [np.array(l) for l in itertools.product([True, False], repeat=len(broken_components_names))]
-        observation_names = [broken_components_names[mask] for mask in masks]
+        self.masks = [np.array(l) for l in itertools.product([True, False], repeat=len(broken_components_names))]
+        observation_names = [broken_components_names[mask] for mask in self.masks]
         observation_space = spaces.Discrete(len(observation_names))
         return observation_space, observation_names
 
@@ -48,31 +55,39 @@ class BrokenComponentsEnv(gym.Env):
 
         return observation_name_dict
 
-    def reset(self, reward_modus: str = 'raw') -> int:
+    def reset(self, reward_modus: str = 'raw'):
+        self.data_handler = DataHandler(data_generation=self.dh_data_generation,
+                                        take_component_id=self.dh_take_component_id,
+                                        transformation=self.reward_modus,
+                                        distinguishable=self.dh_distinguishable)
         self.current_state = 0
         self.current_state_name = list(self.observation_space_names[self.current_state])
         self.last_action = None
         self.last_action_name = None
         self.successful_action = None
         self.steps = 0
-        self.reward_modus = reward_modus
-        return self.current_state
+        
+        if self.state_as_vec:
+            return self.masks[self.current_state].astype(float)
+        else:
+            return self.current_state
     
-    def set_reward_decrease_factor(factor: float) -> None:
+    def set_reward_decrease_factor(self, factor: float) -> None:
         self.reward_decrease_factor = factor
 
     def step(self, action: int) -> Tuple[int, float, bool, dict]:
         self.steps += 1
-        reward = 0
         action_name = self.action_space_names[action]
-        self.successful_action = False
+        self.successful_action = None
 
         if action_name in self.current_state_name:
-            self.successful_action = np.random.uniform() > self.data_handler.get_repair_failure_probability(action_name)
-            if self.successful_action:
+            reward = self.__get_reward(action_name, self.current_state_name)  # returns 0 if it fails
+            if reward > 0:
                 self.current_state_name.remove(action_name)
                 self.current_state = self.observation_name_dict[str(self.current_state_name)]
-                reward = self.__get_reward(action_name)
+                self.successful_action = True
+            else:
+                self.successful_action = False
         else:
             if self.reward_decrease:
                 reward = np.power(self.reward_decrease_factor, self.steps) * self.punishment[self.reward_modus]
@@ -86,14 +101,18 @@ class BrokenComponentsEnv(gym.Env):
 
         self.last_action = action
         self.last_action_name = action_name
-
-        return self.current_state, reward, done, {}
-
-    def __get_reward(self, action_name: Tuple) -> float:
-        if self.reward_decrease:
-            return np.power(self.reward_decrease_factor, self.steps)*self.data_handler.get_reward(action_name, type=self.reward_modus)
+        
+        if self.state_as_vec:
+            return self.masks[self.current_state].astype(float), reward, done, {}
         else:
-            return self.data_handler.get_reward(action_name, type=self.reward_modus)
+            return self.current_state, reward, done, self.masks[self.current_state].astype(float)
+
+    def __get_reward(self, action_name: Tuple, current_state_name: List[Tuple]) -> float:
+        if self.reward_decrease:
+            return np.power(self.reward_decrease_factor, self.steps)*\
+                   self.data_handler.get_reward(action_name, current_state_name)
+        else:
+            return self.data_handler.get_reward(action_name, current_state_name)
 
     def render(self) -> None:
         print('Steps: ', self.steps)
--- envs/data_handler.py
+++ envs/data_handler.py
@@ -1,106 +1,116 @@
 import os
-import sys
 import random
 import pandas as pd
+from envs.environments import check_environment
 import envs.data_utils as du
+from data.transition_matrix.transition import Transition
 
 
 class DataHandler:
 
-    def __init__(self, environment: str = 'Linear'):
+    def __init__(self, data_generation: str = 'Linear', take_component_id: bool = True, transformation: str = 'raw', distinguishable: bool = False):
         '''
-        Initializes the Data Handler by loading data into the environment.
+        Initializes the Data Handler by loading data into the environment and select between using the componenent id or name.
 
-        :param data_function:
-            Choose between the version of the mRubis environment and its function for computing the utility_increase
-            'Linear' (default) | 'Saturating' | 'Combined' | 'Discontinuous'
-            or shifted data
-            'Linear_Shifted'
+        :param data_generation:
+            Choose between the version of the mRubis environment 'Linear' (default) | 'Saturating' | 'Combined' | 'Discontinuous'
+            or shifted data 'LinearShifted'
+            or non_stationary_data choose model like 'ARCH'
+        :param take_component_id:
+            Choose compononent id or name. When take_component_id is false, you will take name.
+        :param transformation:
+            Choose between 'raw' (Default), 'sqt', 'cube', 'log10', 'ln', 'log2'
+        :param distinguishable:
+            Choose only distinguishable <component,failure> pairs by setting this true.
+
+        For all possible combinations of environment, id and type please have a look on the file 'environments.py'.
         '''
-        self.environment = environment
+
+        self.environment, self.filename = check_environment(data_generation, take_component_id, transformation, distinguishable)
+        self.transition = Transition()
         self.data: pd.DataFrame = pd.DataFrame()
         self.component_failure_pairs = []
         self.__load_data()
         self.__create_component_failure_pairs()
 
-    def __prepared_data_file(self) -> str:
-        return 'prepared_data_' + self.environment + '.csv'
-
     def __load_transform_data(self) -> None:
         frames = []
 
         # searching for all csv files in the data directory and loading the data in multiple dataframes
-        for root, dirs, files in os.walk('data'):
+        for root, dirs, files in os.walk('data/original_data'):
             for f in files:
-                if f.endswith(self.environment + '.csv'):
+                if f.endswith(self.environment[0] + '.csv'):
                     file_path = os.path.join(root, f)
                     df = pd.read_csv(file_path)
                     df.columns = df.columns.str.replace('\t', '')
                     frames.append(df)
 
         # combining all df to one df
-        self.data = pd.concat(frames, sort=False)[
+        self.data = pd.concat(frames, sort=False, ignore_index=True)[
             ['Optimal_Affected_Component', 'Optimal_Affected_Component_Uid', 'Optimal_Failure', 'Optimal_Utility_Increase']].rename(columns={'Optimal_Utility_Increase': 'raw'})
+
+        # removing empty rows
+        nan_value = float("NaN")
+        self.data.replace("", nan_value, inplace=True)
+        self.data.dropna(subset=["Optimal_Failure"], inplace=True)
 
         # transform data
         self.data = du.transform_data(self.data)
 
         # save transformed data this to environment as a csv file for quick reload in future
-        self.data.to_csv(self.__prepared_data_file())
+        self.data.to_csv(self.filename)
 
     def __load_data(self):
         try:
-            self.data = pd.read_csv(self.__prepared_data_file(), index_col=0)
+            self.data = pd.read_csv(self.filename, index_col=0)[[self.environment[1], 'Optimal_Failure', self.environment[2]]]
         except FileNotFoundError:
             self.__load_transform_data()
-
-    def get_all_component_failure_pairs(self):
-        return self.component_failure_pairs
-
-    def get_repair_failure_probability(self, component_failure_pair) -> float:
-        return 0.1  # static failure rate
+            self.data = self.data[[self.environment[1], 'Optimal_Failure', self.environment[2]]]
 
     def __create_component_failure_pairs(self) -> None:
-        combinations = self.data.groupby(['Optimal_Affected_Component_Uid', 'Optimal_Failure']).size().reset_index().rename(
+        combinations = self.data.groupby([self.data.columns[0], self.data.columns[1]]).size().reset_index().rename(
             columns={0: 'count'})
         del combinations['count']
         self.component_failure_pairs = [tuple(val) for val in combinations.values]
 
-    def get_sample_component_failure_pairs(self, sample_size: int):
+    def get_all_component_failure_pairs(self):
+        return self.component_failure_pairs
+
+    def get_sample_component_failure_pairs(self, sample_size: int, seed=None):
         if sample_size > len(self.component_failure_pairs):
             print('Error: Sample size exceeds number of (component, failure) pairs.')
         else:
-            return list(random.sample(self.component_failure_pairs, sample_size))
+            return list(set(pd.Series(self.get_all_component_failure_pairs()).sample(sample_size, random_state=seed).tolist()))
 
-    def get_reward(self, component_failure_pair, type: str ='raw') -> float:
+    def get_reward(self, component_failure_pair, list_of_failings) -> float:
+        '''
+        :param component_failure_pair: A tuple of component and failure to repair.
+        :param list_of_failings: A list of tuples of component and failures which are still failing.
+        :return: The reward for the component_failure_pair. A reward of 0 states that the fix fails.
+        '''
         component = component_failure_pair[0]
         failure = component_failure_pair[1]
-        filtered = self.data[
-            (self.data['Optimal_Affected_Component_Uid'] == component) & (self.data['Optimal_Failure'] == failure)]
-        return filtered.sample()[type].values[0]
 
+        # get the failing probability
+        failing_components = list(set([i[0] for i in list_of_failings]))
+        failing_probability = self.transition.return_failing_probability(component, failing_components)
 
-if __name__ == '__main__':
+        # will the fixing fail?
+        if random.random() <= failing_probability:
+            # yes, it fails
+            return 0
+        else:  # no, the fixing will not fail
+            # reduce list of possible rewards to the selection
+            filtered = self.data[
+                (self.data[self.data.columns[0]] == component) & (self.data[self.data.columns[1]] == failure)]
 
-    dh = DataHandler()
-    component_failure_pairs = dh.get_all_component_failure_pairs()
-    print('Selected mRubis environment:', dh.environment)
-    print('Number of <component,failure> pairs:', len(component_failure_pairs))
-    print('Get two samples:', dh.get_sample_component_failure_pairs(2))
-    print('untransformed sampled:', dh.get_reward(component_failure_pairs[0]))
-
-    # Transformation
-    print('Square Root Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='sqt'))
-    print('Cube Root Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='cube'))
-    print('Log10 Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='log10'))
-    print('Ln Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='ln'))
-    print('Log2 Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='log2'))
-
-    # Shifting
-    ordering, shiftedData = du.shift_data(dh.data, 'cube', times=2)
-    result = du.perform_ttest(ordering, shiftedData, 'cube')
-    print('Perform T-Test on shifted data:', (len(result[result['pvalue']<0.025])))
-
-    # Non-stationary series
-    max_min_std_values = dh.data[['Optimal_Affected_Component_Uid', 'Optimal_Failure', 'raw']].groupby(['Optimal_Affected_Component_Uid', 'Optimal_Failure'])['raw'].agg([max, min, 'std']).reset_index()
-    print('Create AR non-stationary time series', du.create_non_stationary_data(max_min_std_values, du.ARCH, N=100))
+            # select a reward
+            sample_value = 0
+            if self.environment[0] in ['ARol', 'GARCH']:
+                # in the non-stationary enviornment a already used value is removed from the list of possible reward values
+                sample_value = filtered.iloc[0][filtered.columns[2]]
+                index = filtered.index[0]
+                self.data = self.data.drop(index)
+            else:
+                sample_value = filtered.sample()[self.data.columns[2]].values[0]
+            return sample_value
